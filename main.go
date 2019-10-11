bcc9d386bb7ac8feec4897788bce36a5b62e88e7
Shehanka <hcsperera@outlook.com>
2019-10-11 16:06:27 +0530

    Docker file added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"fmt"
 )
@@ -9,6 +10,8 @@ func main()  {
 	fmt.Println("Go Docker!!")
 
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request)  {
-		
+		fmt.Fprintf(w, "Hello world")
 	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))	
 }
